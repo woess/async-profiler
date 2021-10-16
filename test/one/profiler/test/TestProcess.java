555a7211a4d1eeb7ae8dba108b66f19924bb072d
Andrei Pangin <noreply@pangin.pro>
2021-10-16 22:46:06 +0300

    Test framework

--- test/one/profiler/test/TestProcess.java
+++ test/one/profiler/test/TestProcess.java
@@ -1,18 +1,46 @@
+/*
+ * Copyright 2021 Andrei Pangin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package one.profiler.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestProcess implements AutoCloseable {
+    private static final Logger log = Logger.getLogger(TestProcess.class.getName());
+
+    private static final Pattern filePattern = Pattern.compile("%[a-z]+");
+
     private static final MethodHandle pid = getPidHandle();
 
     private static MethodHandle getPidHandle() {
@@ -34,82 +62,120 @@ public class TestProcess implements AutoCloseable {
     }
 
     private final Process p;
+    private final Map<String, File> tmpFiles = new HashMap<>();
+    private int timeout = 30;
 
-    public TestProcess(Process p) {
-        this.p = p;
-    }
-
-    private static void addArgs(List<String> cmd, String args) {
-        if (args != null && !args.isEmpty()) {
-            Collections.addAll(cmd, args.split(" "));
-        }
-    }
-
-    public static TestProcess runJava(Class<?> mainClass, String jvmArgs) throws IOException, InterruptedException {
-        return runJava(mainClass.getName(), jvmArgs);
-    }
-
-    public static TestProcess runJava(String mainClass, String jvmArgs) throws IOException, InterruptedException {
+    public TestProcess(String mainClass, String jvmArgs) throws IOException {
         List<String> cmd = new ArrayList<>();
         cmd.add(System.getProperty("java.home") + "/bin/java");
         cmd.add("-cp");
-        cmd.add("out/production/async-profiler");
+        cmd.add(System.getProperty("java.class.path"));
         addArgs(cmd, jvmArgs);
         cmd.add(mainClass);
-        System.out.println(cmd);
+        log.info("Running " + cmd);
 
-        Process p = new ProcessBuilder(cmd)
+        this.p = new ProcessBuilder(cmd)
                 .inheritIO()
                 .start();
+    }
 
-        Thread.sleep(100);
-        return new TestProcess(p);
+    private void addArgs(List<String> cmd, String args) {
+        if (args != null && !args.isEmpty()) {
+            args = substituteFiles(args);
+            for (StringTokenizer st = new StringTokenizer(args, " "); st.hasMoreTokens(); ) {
+                cmd.add(st.nextToken());
+            }
+        }
+    }
+
+    private String substituteFiles(String s) {
+        Matcher m = filePattern.matcher(s);
+        if (!m.find()) {
+            return s;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        do {
+            File f = createTempFile(m.group());
+            m.appendReplacement(sb, f.toString());
+        } while (m.find());
+
+        m.appendTail(sb);
+        return sb.toString();
+    }
+
+    private File createTempFile(String id) {
+        try {
+            File f = File.createTempFile("ap-" + id.substring(1), ".tmp");
+            tmpFiles.put(id, f);
+            return f;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Override
     public void close() throws TimeoutException, InterruptedException {
         p.destroy();
-        if (!p.waitFor(5, TimeUnit.SECONDS)) {
-            p.destroyForcibly();
-            throw new TimeoutException("Child process has not exited");
+        try {
+            waitForExit(p, 5);
+        } finally {
+            for (File file : tmpFiles.values()) {
+                 file.delete();
+            }
         }
     }
 
     public long pid() {
         try {
-            return (long) pid.invokeExact((Process) p);
+            return (long) pid.invokeExact(p);
         } catch (Throwable e) {
             throw new IllegalStateException("Unsupported API", e);
         }
     }
 
-    public File profile(String args) throws IOException, TimeoutException, InterruptedException {
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public void waitForExit() throws TimeoutException, InterruptedException {
+        waitForExit(p, timeout);
+    }
+
+    private void waitForExit(Process p, int seconds) throws TimeoutException, InterruptedException {
+        if (!p.waitFor(seconds, TimeUnit.SECONDS)) {
+            p.destroyForcibly();
+            throw new TimeoutException("Child process has not exited");
+        }
+    }
+
+    public List<String> profile(String args) throws IOException, TimeoutException, InterruptedException {
+        // Give JVM process some time to initialize
+        Thread.sleep(100);
+
         List<String> cmd = new ArrayList<>();
         cmd.add("/bin/sh");
         cmd.add("profiler.sh");
         addArgs(cmd, args);
         cmd.add(Long.toString(pid()));
-        System.out.println(cmd);
-
-        File out = File.createTempFile("ap-out", ".tmp");
+        log.info("Profiling " + cmd);
 
         Process p = new ProcessBuilder(cmd)
-                .inheritIO()
-                .redirectOutput(out)
+                .redirectOutput(createTempFile("%out"))
+                .redirectError(createTempFile("%err"))
                 .start();
 
-        if (!p.waitFor(30, TimeUnit.SECONDS)) {
-            p.destroyForcibly();
-            throw new TimeoutException("Profile process has not exited");
-        }
+        waitForExit(p, timeout);
 
-        return out;
+        return readFile("%out");
     }
 
-    public static void main(String[] args) throws Exception {
-        try (TestProcess p = runJava(test.smoke.Cpu.class, "")) {
-            File out = p.profile("-d 5 -e cpu -o collapsed");
-            System.out.println(out);
+    public List<String> readFile(String id) {
+        try {
+            File f = tmpFiles.get(id);
+            return Files.readAllLines(f.toPath());
+        } catch (IOException e) {
+            return Collections.emptyList();
         }
     }
 }
